8ce4c44a0edef80ed5f1420c6d1254b4ddf557bf
Aarush Karak <worldshaans@gmail.com>
2026-08-01 15:06:49 +0000

    Update coderbros site: 2026-08-01 15:06 [skip ci]

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,4 +1,4 @@
-// Updated: 2026-08-01 13:43:18
+// Updated: 2026-08-01 15:06:49
 "use client";
 
 export default function Error({
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,4 @@
-// Updated: 2026-08-01 13:43:18
+// Updated: 2026-08-01 15:06:49
 export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#0a0a0a]">
