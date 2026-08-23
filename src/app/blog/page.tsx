df215fc0e7cac92cc1af6afcfb22cff27ee9383a
Aarush Karak <worldshaans@gmail.com>
2026-08-23 14:24:33 -0400

    fix: reduce blog thumbnail gradient overlay opacity

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -35,7 +35,7 @@ export default function BlogPage() {
                         sizes="(min-width: 640px) 176px, 144px"
                         className="object-cover transition-transform duration-500 group-hover:scale-105"
                       />
-                      <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent" />
+                      <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent" />
                     </div>
                   )}
                   <div className="min-w-0">
