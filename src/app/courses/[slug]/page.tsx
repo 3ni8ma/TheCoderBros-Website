e88a1b2aea5d23444583b69c63b2855104d133a9
Aarush Karak <worldshaans@gmail.com>
2026-08-23 13:49:33 -0400

    fix: correct courses import in course page

--- src/app/courses/[slug]/page.tsx
+++ src/app/courses/[slug]/page.tsx
@@ -1,5 +1,5 @@
 // Updated: 2026-08-23 13:30:04
-import { courses } from "@/lib/data/courses";
+import { languages as courses } from "@/lib/data/courses";
 import { notFound } from "next/navigation";
 import Slideshow from "@/components/courses/slideshow";
 
