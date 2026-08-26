954daf8ec9172e87429ab5a1b56bf27a9eb2e513
Aarush Karak <worldshaans@gmail.com>
2026-08-26 15:21:29 -0400

    Update coderbros site: 2026-08-26 15:30 [skip ci]

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,4 @@
-// Updated: 2026-08-26 15:00:09
+// Updated: 2026-08-26 15:30:02
 export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#0a0a0a]">
