0eadc74a9f0fa880699d0ca44b2ccb5b05ef4336
Aarush Karak <worldshaans@gmail.com>
2026-09-05 23:37:17 -0400

    Update coderbros site: 2026-09-06 00:00 [skip ci]

--- src/app/courses/[slug]/page.tsx
+++ src/app/courses/[slug]/page.tsx
@@ -1,4 +1,4 @@
-// Updated: 2026-09-05 23:30:22
+// Updated: 2026-09-06 00:00:04
 import { languages as courses } from "@/lib/data/courses";
 import { notFound } from "next/navigation";
 import Slideshow from "@/components/courses/slideshow";
