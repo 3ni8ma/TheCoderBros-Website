5ab311e82b64fd0b47236de9fdad4071c5edeed7
Aarush Karak <worldshaans@gmail.com>
2026-09-07 14:07:05 -0400

    Update coderbros site: 2026-09-07 15:00 [skip ci]

--- src/app/courses/[slug]/page.tsx
+++ src/app/courses/[slug]/page.tsx
@@ -1,4 +1,4 @@
-// Updated: 2026-09-07 14:30:18
+// Updated: 2026-09-07 15:00:31
 import { languages as courses } from "@/lib/data/courses";
 import { notFound } from "next/navigation";
 import Slideshow from "@/components/courses/slideshow";
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,4 @@
-// Updated: 2026-09-07 14:30:18
+// Updated: 2026-09-07 15:00:31
 export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#0a0a0a]">
